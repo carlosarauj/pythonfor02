3e730eed1d58a24c408956058123172c7cf3ebf9
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-05-07 20:07:28 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,26 @@
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0],
+]
+
+for linha in matrix:
+    # print(linha)
+    print("------")
+    for coluna in linha:
+        print(coluna)
+
+# nomes = ["Carlos", "Beto", "Alfredo"]
+
+
+# for index in range(len(nomes)):
+#     print(nomes[index],index)
+
+# for index in range(5):
+#     if index == 0:
+#         print("primeira linha")
+#     else:
+#         print(f"linha {index}")
+
+#usa o terminal com: python nomedoarquivo.py
